9c3b519c5fd186b5ebe37992ab97b1a1be509fea
simboslice <simeonadda@gmail.com>
2021-08-07 01:32:06 -0500

    animation success

--- js/app.js
+++ js/app.js
@@ -8,58 +8,191 @@ inputName.addEventListener("click", (event) => {
   document.querySelector("h2").innerHTML = `${charName}`
 })
 
+function hideForm() {
+  const form = document.querySelector("#form")
+  form.style.display = 'none'
+  showStart()
+}
+
 let petName = document.querySelector("h2").innerHTML
 
 // Tomagotchi class
 class Tomagotchi {
   constructor(name) {
     this.name = name
-    this.hunger = 1
-    this.sleepiness = 1
-    this.boredom = 1
+    this.hunger = 0
+    this.sleepiness = 0
+    this.boredom = 0
     this.age = 1
     this.dead = false
+    this.time = 0
   }
   getOlder() {
-    setInterval(() => {
-      age.innerHTML = `Age: ${this.age}`
+    let gameImage = document.querySelector(".game-image")
+    let count = setInterval(() => {
       this.age++
-    }, 15000)
+      age.innerHTML = `Age: ${this.age}`
 
-    console.log(this.age);
+      if (this.age >= 5) {
+        gameImage.src = "Tomagotchi Images/transform bot 1.jpg"
+      }
+
+      if (this.age >= 15) {
+        gameImage.src = "Tomagotchi Images/transform bot 2.jpg"
+      }
+
+      if (this.age === 25) {
+        clearInterval(count)
+        this.dead = true
+        gameImage.src = "Tomagotchi Images/death bot.jpg"
+        endGame()
+      }
+    }, 10000)
   }
   getHungry() {
-    setInterval(() => {
-      hunger.innerHTML = `Hunger: ${this.hunger}`
+    let gameImage = document.querySelector(".game-image")
+    let count = setInterval(() => {
       this.hunger++
-    }, 3000)
+      hunger.innerHTML = `Hunger: ${this.hunger}`
+
+      if (this.hunger > 4) {
+        gameImage.src = "Tomagotchi Images/sad bot.jpg"
+      }
+
+      if (this.hunger > 7) {
+        gameImage.src = "Tomagotchi Images/mad bot.jpg"
+      }
+
+      if (this.hunger === 10) {
+        clearInterval(count)
+        gameImage.src = "Tomagotchi Images/death bot.jpg"
+        endGame()
+      }
+    }, 1000)
   }
   getSleepy() {
-    setInterval(() => {
-      sleepiness.innerHTML = `Sleepiness: ${this.sleepiness}`
+    let gameImage = document.querySelector(".game-image")
+    let count = setInterval(() => {
       this.sleepiness++
+      sleepiness.innerHTML = `Sleepiness: ${this.sleepiness}`
+
+      if (this.sleepiness > 4) {
+        gameImage.src = "Tomagotchi Images/sad bot.jpg"
+      }
+
+      if (this.sleepiness > 7) {
+        gameImage.src = "Tomagotchi Images/exclamation bot.jpg"
+      }
+
+      if (this.sleepiness === 10) {
+        clearInterval(count)
+        gameImage.src = "Tomagotchi Images/death bot.jpg"
+        endGame()
+      }
     }, 5000)
   }
   getBored() {
-    setInterval(() => {
-      boredom.innerHTML = `Boredom: ${this.boredom}`
+    let gameImage = document.querySelector(".game-image")
+    let count = setInterval(() => {
       this.boredom++
+      boredom.innerHTML = `Boredom: ${this.boredom}`
+
+      if (this.boredom > 4) {
+        gameImage.src = "Tomagotchi Images/sad bot.jpg"
+      }
+
+      if (this.boredom > 7) {
+        gameImage.src = "Tomagotchi Images/exclamation bot.jpg"
+      }
+
+      if (this.boredom === 10) {
+        clearInterval(count)
+        gameImage.src = "Tomagotchi Images/death bot.jpg"
+        endGame()
+      }
+    }, 3000)
+  }
+  play() {
+    this.boredom = 0
+    this.hunger++
+    let gameImage = document.querySelector(".game-image")
+    gameImage.src = "Tomagotchi Images/excited bot.jpg"
+    setTimeout(() => {
+      gameImage.src = "Tomagotchi Images/happy bot.jpg"
+
+      if (this.age >= 5) {
+        gameImage.src = "Tomagotchi Images/transform bot 1.jpg"
+      }
+
+      if (this.age >= 15) {
+        gameImage.src = "Tomagotchi Images/transform bot 2.jpg"
+      }
+    }, 2500)
+  }
+  eat() {
+    this.hunger = 0
+    let gameImage = document.querySelector(".game-image")
+    gameImage.src = "Tomagotchi Images/food.jpg"
+    setTimeout(() => {
+      gameImage.src = "Tomagotchi Images/happy bot.jpg"
+
+      if (this.age >= 5) {
+        gameImage.src = "Tomagotchi Images/transform bot 1.jpg"
+      }
+
+      if (this.age >= 15) {
+        gameImage.src = "Tomagotchi Images/transform bot 2.jpg"
+      }
+    }, 2500)
+  }
+  sleep() {
+    this.sleepiness = 0
+    let gameImage = document.querySelector(".game-image")
+    gameImage.src = "Tomagotchi Images/sleep sheep.jpg"
+    setTimeout(() => {
+      gameImage.src = "Tomagotchi Images/happy bot.jpg"
+
+      if (this.age >= 5) {
+        gameImage.src = "Tomagotchi Images/transform bot 1.jpg"
+      }
+
+      if (this.age >= 15) {
+        gameImage.src = "Tomagotchi Images/transform bot 2.jpg"
+      }
     }, 2500)
   }
   exercise() {
+    this.boredom = 0
+    let gameImage = document.querySelector(".game-image")
+    gameImage.src = "Tomagotchi Images/exercise.jpg"
+    setTimeout(() => {
+      gameImage.src = "Tomagotchi Images/happy bot.jpg"
 
+      if (this.age >= 5) {
+        gameImage.src = "Tomagotchi Images/transform bot 1.jpg"
+      }
+
+      if (this.age >= 15) {
+        gameImage.src = "Tomagotchi Images/transform bot 2.jpg"
+      }
+    }, 2500)
   }
   move() {
-
   }
 }
 
+const pet = new Tomagotchi(petName)
+
 function startGame() {
-  const pet = new Tomagotchi(petName)
   pet.getOlder()
   pet.getHungry()
   pet.getSleepy()
   pet.getBored()
+  pet.move()
+}
+
+function endGame() {
+  return
 }
 
 
@@ -71,32 +204,39 @@ const exerciseButton = document.querySelector("#exercise")
 
 // Game button event listerners
 playButton.addEventListener('click', function() {
+  pet.play()
 
+  document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/4199031.jpg')";
 });
 
 eatButton.addEventListener('click', function() {
+  pet.eat()
 
+  document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/4199031.jpg')";
 });
 
 sleepButton.addEventListener('click', function() {
+  pet.sleep()
+
   document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1475274047050-1d0c0975c63e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80')";
 });
 
 exerciseButton.addEventListener('click', function() {
+  pet.exercise()
 
+  document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/4199031.jpg')";
 })
 
 // Attributes
 const age = document.querySelector("#age")
-age.innerHTML = `Age`
-
 const hunger = document.querySelector("#hunger")
-hunger.innerHTML = `Hunger`
-
 const sleepiness = document.querySelector("#sleepiness")
-sleepiness.innerHTML = `Sleepiness`
-
 const boredom = document.querySelector("#boredom")
-boredom.innerHTML = `Boredom`
 
-startGame()
+// Start Button
+const startButton = document.querySelector("#start-button")
+startButton.addEventListener('click' , startGame)
+
+function showStart() {
+  startButton.style.display = 'initial'
+}
